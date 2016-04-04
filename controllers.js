d07328bb020c45608da1e98bdb330384d642bb5a
S.SKNS <somtida.th@gmail.com>
2016-04-04 01:00:30 -0700

    AngularJs

---
+++ controllers.js
@@ -0,0 +1,23 @@
+myapp.controller("homeController",['$scope','cityService',function($scope,cityService){
+    $scope.city=cityService.city;
+    $scope.$watch('city',function(){
+        cityService.city=$scope.city;
+    });
+}]);
+myapp.controller("forecastController",['$scope','$resource','$routeParams','cityService',function($scope,$resource,$routeParams,cityService){
+    $scope.city=cityService.city; 
+    
+    $scope.days=$routeParams.days || '5';
+    
+    $scope.weatherAPI=$resource("http://api.openweathermap.org/data/2.5/forecast/daily",{ callback: "JSON_CALLBACK"},{get:{method:"JSONP"}});
+    
+    $scope.weatherResult=$scope.weatherAPI.get({q:$scope.city, cnt:$scope.days, appid:'c02e0325ef8295b236a3054bb520cb4b'});
+    //console.log($scope.weatherResult);
+    $scope.convertToF=function(degK){
+        return Math.round((1.8*(degK-273))+32);
+    }
+    
+    $scope.convertToDate=function(dt){
+        return new Date(dt*1000);
+    }
+}]);
